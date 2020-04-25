696dcd9fdbcd786a55e4fb106f95b3694abb2830
Depak Borhara <depakborhara@gmail.com>
2020-04-25 01:27:17 -0400

    Added shop page, collection preview component, and collection item component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import './App.css';
 
 //Import Components
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
