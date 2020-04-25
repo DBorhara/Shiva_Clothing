11974abb0a228cb326415f38e24f7146b91ed6fb
Depak Borhara <depakborhara@gmail.com>
2020-04-24 23:39:17 -0400

    Added basic routing with reactrouterdom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
+//Import Components
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
+
+//Import Component
+import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends Component {
   constructor(props) {
@@ -11,33 +13,33 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats',
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'shop/jackets',
+          linkUrl: '',
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: 'shop/sneakers',
+          linkUrl: '',
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/womens',
+          linkUrl: '',
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/mens',
+          linkUrl: '',
         },
       ],
     };
@@ -46,8 +48,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.tiles.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.tiles.map(({ id, ...otherTileProps }) => (
+          <MenuItem key={id} {...otherTileProps} />
         ))}
       </div>
     );
