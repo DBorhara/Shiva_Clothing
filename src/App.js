bdd89e31bc0afbe3a412aafad4eebf1d8bc5d839
Depak Borhara <depakborhara@gmail.com>
2020-04-25 02:54:46 -0400

    Header added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 //Import Components
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
