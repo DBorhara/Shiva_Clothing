0539f1ae733f971b01483b696568d84d5978428d
Depak Borhara <depakborhara@gmail.com>
2020-04-27 00:06:03 -0400

    Email/Password sign in completed

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor(props) {
@@ -21,10 +21,20 @@ class SignIn extends Component {
     //Dynamically setting property of name with [name] for email and password
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: '',
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,7 +14,6 @@ const config = {
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  console.log(userAuth);
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
