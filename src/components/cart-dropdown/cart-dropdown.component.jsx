1551f3a001566e1bea969ad42a709cb5d609712b
Depak Borhara <depakborhara@gmail.com>
2020-04-28 22:53:41 -0400

    Can add to dropdown TODO: fix styling for dropdown box

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-item"></div>
+    <div className="cart-item">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>CHECKOUT NOW</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
