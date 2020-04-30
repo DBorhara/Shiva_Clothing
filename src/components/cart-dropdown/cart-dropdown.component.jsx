8907956d57437105c707bd77fde3d0e801121d56
Depak Borhara <depakborhara@gmail.com>
2020-04-29 21:14:12 -0400

    Fixed cart displaty bug

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,7 @@ import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-item">
+    <div className="cart-items">
       {cartItems.map((cartItem) => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
